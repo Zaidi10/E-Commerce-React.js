5465be88b02eca95b6ff67c6fc227e046bdc5303
Zaidi10 <16ume051@lnmiit.ac.in>
2020-07-10 14:45:51 +0530

    commit 2

---
+++ build/precache-manifest.000230d26490736479ac0a0dc035f59f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db95abedec231bacc0e74cfbed221f30",
+    "url": "/E-Commerce-React.js/index.html"
+  },
+  {
+    "revision": "37cffbcbf0ed90158d6a",
+    "url": "/E-Commerce-React.js/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "652143fc6c91ad133ac9",
+    "url": "/E-Commerce-React.js/static/js/2.50933d71.chunk.js"
+  },
+  {
+    "revision": "37cffbcbf0ed90158d6a",
+    "url": "/E-Commerce-React.js/static/js/main.397df629.chunk.js"
+  },
+  {
+    "revision": "ff8e938c5c24bb5fa356",
+    "url": "/E-Commerce-React.js/static/js/runtime~main.b23ad236.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/E-Commerce-React.js/precache-manifest.419326b79f5a0a4fd94286c7f95c37f6.js"
+  "/E-Commerce-React.js/precache-manifest.000230d26490736479ac0a0dc035f59f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/E-Commerce-React.js/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
