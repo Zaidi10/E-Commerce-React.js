0ca8b1db619c825bb0bbe9b962748d7c24a48850
Zaidi10 <16ume051@lnmiit.ac.in>
2020-07-10 15:57:38 +0530

    Updates

---
+++ precache-manifest.3565b516ce11fb3595a32a48018ca237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c782223b5398494a650b50f1513d1414",
+    "url": "/E-Commerce-React.js/index.html"
+  },
+  {
+    "revision": "cb73d3f98fa1aaafe4ac",
+    "url": "/E-Commerce-React.js/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "e4c7b476da9f2052166f",
+    "url": "/E-Commerce-React.js/static/js/2.2f3ffe3c.chunk.js"
+  },
+  {
+    "revision": "cb73d3f98fa1aaafe4ac",
+    "url": "/E-Commerce-React.js/static/js/main.e3f959cc.chunk.js"
+  },
+  {
+    "revision": "94242b3f8b7ab1555ffd",
+    "url": "/E-Commerce-React.js/static/js/runtime~main.47bafb02.js"
+  }
+]);
